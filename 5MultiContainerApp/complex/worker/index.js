d75029de8f671661f986891eb12489ce6c65ba04
projectHolsey <matty-sep@hotmail.com>
2024-12-17 10:25:27 -0500

    Made node project and added development docker files for each section - details in explanation.txt

--- 5MultiContainerApp/complex/worker/index.js
+++ 5MultiContainerApp/complex/worker/index.js
@@ -1,27 +1,19 @@
-// Getting the host / port number we require
-const keys = require("./keys") 
-// importing the redis library
-const redis = require("redis")
+const keys = require('./keys');
+const redis = require('redis');
 
-// Creating the client to connect to the redis server
-const redisClient = redis.createClients({
-    host: keys.redisHost, // use the host ip provided
-    port: keys.redisPort, // use the port provided
-    retry_strategy: () => 1000 // retry... unsure
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
+const sub = redisClient.duplicate();
 
-// Creating a second version of the redis client called sub
-const sub = redisClient.duplicate()
-
-// Fibinnaci sequence function (slow)
-function fib(index){
-    if (index < 2) return 1;
-
-    return fib(index - 1) + fib(index - 2);
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-// On new message, use the redis client to store the index provided
-sub.on('messsage', (channel, message) => {
-    redisClient.hset('values', message, fib(parseInt(message)))
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-sub.subscribe('insert')
+sub.subscribe('insert');
